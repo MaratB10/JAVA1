c8874400e0625af339520dc1734782d76fb25acd
MaratB10 <bmt1986@yandex.ru>
2020-08-28 00:18:06 +0300

    new

--- src/homework2/task1.java
+++ src/homework2/task1.java
@@ -1,4 +1,30 @@
 package homework2;
 
 public class task1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,1,0,0,1,0,0,1,0};
+
+        System.out.print("old array:");
+        for (int value : arr) {
+            System.out.print(" " + value);
+        }
+
+        System.out.println();
+
+        System.out.print("new array:");
+        for (int value : arr) {
+            if (value == 0) {
+                value = 1;
+            } else value = 0;
+            System.out.print(" " + value);
+        }
+//        for (int i = 0; i < arr.length; i++){
+//            if (arr[i] == 0) {
+//                arr[i] = 1;
+//            } else arr[i] = 0;
+//            System.out.print(" " + arr[i]);
+//        }
+    }
 }
